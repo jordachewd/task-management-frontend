5299ff531553d86c63049c748df8a2c0f11fc268
FlorinIO <florin.iordache@jordachewd.ro>
2024-12-19 23:06:35 +0200

    @babel/plugin-proposal-private-property-in-object and axios fixes

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,16 +1,24 @@
-import { post } from 'axios';
-import BaseHttpService from './base-http.service';
+//import { post } from 'axios';
+import axios from "axios";
+import BaseHttpService from "./base-http.service";
 
 export default class AuthService extends BaseHttpService {
   async signin(username, password) {
-    const result = await post(`${this.BASE_URL}/auth/signin`, { username, password });
+    //const result = await post(`${this.BASE_URL}/auth/signin`, { username, password });
+
+    const result = axios.post(`${this.BASE_URL}/auth/signin`, {
+      username,
+      password,
+    });
+
     const accessToken = result.data.accessToken;
     this.saveToken(accessToken);
     return result.data.username;
   }
 
   async signup(username, password) {
-    await post(`${this.BASE_URL}/auth/signup`, { username, password });
+    //await post(`${this.BASE_URL}/auth/signup`, { username, password });
+    axios.post(`${this.BASE_URL}/auth/signup`, { username, password });
   }
 
   async signout() {
